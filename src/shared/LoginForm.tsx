79a31dac88729156179745573a3b693772f893fe
ginpei <ginpei@gmail.com>
2021-02-06 22:15:06 -0800

    fix: 🐛 avoid no callbacks warning

--- src/shared/LoginForm.tsx
+++ src/shared/LoginForm.tsx
@@ -2,7 +2,12 @@ import firebase from "firebase/app";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import { auth } from "../models/firebase";
 
-const uiConfig = {
+const uiConfig: firebaseui.auth.Config = {
+  callbacks: {
+    signInSuccessWithAuthResult() {
+      return false;
+    },
+  },
   signInFlow: "popup",
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
