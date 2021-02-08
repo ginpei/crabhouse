0708c87fd0d107c6e0e13e724bbd6a8d8559d3a0
ginpei <ginpei@gmail.com>
2021-02-07 17:46:34 -0800

    feat: 🎸 basic header

--- src/screens/shared/BasicLayout.tsx
+++ src/screens/shared/BasicLayout.tsx
@@ -1,16 +1,23 @@
 import { HTMLProps } from "react";
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 import { jcn } from "../../misc/misc";
+import "./BasicLayout.scss";
 
 export const BasicLayout: React.FC<
   HTMLProps<HTMLDivElement> & { className: string; title: string }
 > = ({ className, children, title, ...props }) => {
   return (
-    <div {...props} className={jcn(className, "BasicLayout ui-container")}>
+    <div {...props} className={jcn(className, "BasicLayout")}>
       <Helmet>
         <title>{title} - Crabhouse</title>
       </Helmet>
-      {children}
+      <header className="BasicLayout-header">
+        <div className="ui-container">
+          <Link to="/">🦀 Crabhouse</Link>
+        </div>
+      </header>
+      <div className="ui-container">{children}</div>
     </div>
   );
 };
