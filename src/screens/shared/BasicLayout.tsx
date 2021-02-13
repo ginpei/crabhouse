07275de5035cae23ebb3b607f2e2af5bc880705f
ginpei <ginpei@gmail.com>
2021-02-13 11:22:19 -0800

    feat: 🎸 link to my page if logged in

--- src/screens/shared/BasicLayout.tsx
+++ src/screens/shared/BasicLayout.tsx
@@ -1,13 +1,23 @@
 import { HTMLProps } from "react";
 import { Helmet } from "react-helmet-async";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { jcn } from "../../misc/misc";
+import { AppState } from "../../stores/appStore";
+import { myPagePath } from "../my/MyPage";
 import { BasicHeaderFrame } from "./BasicHeaderFrame";
 import "./BasicLayout.scss";
 
-export const BasicLayout: React.FC<
-  HTMLProps<HTMLDivElement> & { className: string; title: string }
-> = ({ className, children, title, ...props }) => {
+const mapState = (state: AppState) => ({
+  currentUserId: state.currentUserId,
+});
+
+const BasicLayoutBase: React.FC<
+  HTMLProps<HTMLDivElement> &
+    ReturnType<typeof mapState> & { className: string; title: string }
+> = ({ className, children, currentUserId, title, ...props }) => {
+  const logoLinkUrl = currentUserId ? myPagePath() : "/";
+
   return (
     <div {...props} className={jcn(className, "BasicLayout")}>
       <Helmet>
@@ -15,10 +25,12 @@ export const BasicLayout: React.FC<
       </Helmet>
       <BasicHeaderFrame>
         <div className="ui-container">
-          <Link to="/">🦀 Crabhouse</Link>
+          <Link to={logoLinkUrl}>🦀 Crabhouse</Link>
         </div>
       </BasicHeaderFrame>
       <div className="ui-container">{children}</div>
     </div>
   );
 };
+
+export const BasicLayout = connect(mapState)(BasicLayoutBase);
