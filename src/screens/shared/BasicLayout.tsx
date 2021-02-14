c0a6bf02f0729bc4d224082245a86b17cecb0b9a
ginpei <ginpei@gmail.com>
2021-02-13 22:41:17 -0800

    Revert "feat: 🎸 link to my page if logged in"
    This reverts commit 07275de5035cae23ebb3b607f2e2af5bc880705f.

--- src/screens/shared/BasicLayout.tsx
+++ src/screens/shared/BasicLayout.tsx
@@ -1,23 +1,13 @@
 import { HTMLProps } from "react";
 import { Helmet } from "react-helmet-async";
-import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { jcn } from "../../misc/misc";
-import { AppState } from "../../stores/appStore";
-import { myPagePath } from "../my/MyPage";
 import { BasicHeaderFrame } from "./BasicHeaderFrame";
 import "./BasicLayout.scss";
 
-const mapState = (state: AppState) => ({
-  currentUserId: state.currentUserId,
-});
-
-const BasicLayoutBase: React.FC<
-  HTMLProps<HTMLDivElement> &
-    ReturnType<typeof mapState> & { className: string; title: string }
-> = ({ className, children, currentUserId, title, ...props }) => {
-  const logoLinkUrl = currentUserId ? myPagePath() : "/";
-
+export const BasicLayout: React.FC<
+  HTMLProps<HTMLDivElement> & { className: string; title: string }
+> = ({ className, children, title, ...props }) => {
   return (
     <div {...props} className={jcn(className, "BasicLayout")}>
       <Helmet>
@@ -25,12 +15,10 @@ const BasicLayoutBase: React.FC<
       </Helmet>
       <BasicHeaderFrame>
         <div className="ui-container">
-          <Link to={logoLinkUrl}>🦀 Crabhouse</Link>
+          <Link to="/">🦀 Crabhouse</Link>
         </div>
       </BasicHeaderFrame>
       <div className="ui-container">{children}</div>
     </div>
   );
 };
-
-export const BasicLayout = connect(mapState)(BasicLayoutBase);
