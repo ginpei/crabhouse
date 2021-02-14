b36aa077f13afec5dd752c82c04b4bafee4044b3
ginpei <ginpei@gmail.com>
2021-02-13 21:45:18 -0800

    refactor: 💡 extract SessionControls

---
+++ src/shared/standalone/SessionControls.tsx
@@ -0,0 +1,87 @@
+import { Dispatch } from "@reduxjs/toolkit";
+import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { myRoomPagePath } from "../../screens/my/room/MyRoomPage";
+import {
+  leaveAgoraChannel,
+  useAgoraChannelJoined,
+  useAgoraClient,
+} from "../../stores/agora";
+import { appSlice, AppState } from "../../stores/appStore";
+import { useCurrentUserStore } from "../../stores/currentUser";
+
+const mapControlsState = (state: AppState) => ({
+  currentUserId: state.currentUserId,
+  playingSession: state.participatingSession,
+  sessionPlayerVisible: state.sessionPlayerVisible,
+});
+
+const mapControlsDispatch = (dispatch: Dispatch) => ({
+  hideSessionPlayer: () => dispatch(appSlice.actions.hideSessionPlayer()),
+});
+
+const SessionControlsBase: React.FC<
+  ReturnType<typeof mapControlsState> & ReturnType<typeof mapControlsDispatch>
+> = ({ currentUserId, hideSessionPlayer, playingSession }) => {
+  useCurrentUserStore();
+  const history = useHistory();
+
+  const agoraClient = useAgoraClient();
+  const [listening, left] = useAgoraChannelJoined(agoraClient);
+
+  const ownRoom = playingSession?.id === currentUserId;
+
+  const onMyPageClick = () => {
+    history.push(myRoomPagePath());
+  };
+
+  const onStopClick = () => {
+    leaveAgoraChannel(agoraClient);
+  };
+
+  const onCloseClick = () => {
+    hideSessionPlayer();
+  };
+
+  return (
+    <div className="SessionPlayer-controls">
+      {ownRoom && (listening || left) && (
+        <button
+          className="SessionPlayer-button"
+          onClick={onMyPageClick}
+          title="My page"
+        >
+          🏠
+        </button>
+      )}
+      {!ownRoom && listening && (
+        <button
+          className="SessionPlayer-button"
+          onClick={onStopClick}
+          title="Play"
+        >
+          ⏹
+        </button>
+      )}
+      {!ownRoom && left && (
+        <button
+          className="SessionPlayer-button"
+          onClick={onCloseClick}
+          title="Close"
+        >
+          &times;
+        </button>
+      )}
+      {!listening && !left && (
+        <button className="SessionPlayer-button" disabled>
+          <span className="SessionPlayer-spinner">*</span>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export const SessionControls = connect(
+  mapControlsState,
+  mapControlsDispatch
+)(SessionControlsBase);
--- src/shared/standalone/SessionPlayer.tsx
+++ src/shared/standalone/SessionPlayer.tsx
@@ -1,59 +1,22 @@
-import { Dispatch } from "@reduxjs/toolkit";
 import { connect } from "react-redux";
-import { Link, useHistory } from "react-router-dom";
-import { noop } from "../../misc/misc";
-import { myRoomPagePath } from "../../screens/my/room/MyRoomPage";
+import { Link } from "react-router-dom";
 import { userRoomPagePath } from "../../screens/users/room/UserRoomPage";
-import {
-  leaveAgoraChannel,
-  useAgoraChannelJoined,
-  useAgoraClient,
-  useAgoraSound,
-} from "../../stores/agora";
-import { appSlice, AppState } from "../../stores/appStore";
+import { useAgoraClient, useAgoraSound } from "../../stores/agora";
+import { AppState } from "../../stores/appStore";
+import { SessionControls } from "./SessionControls";
 import "./SessionPlayer.scss";
 
-// TODO
-const usePlayingRoomStore = noop;
-
 const mapState = (state: AppState) => ({
-  currentUserId: state.currentUserId,
   playingSession: state.participatingSession,
   sessionPlayerVisible: state.sessionPlayerVisible,
 });
 
-const mapDispatch = (dispatch: Dispatch) => ({
-  hideSessionPlayer: () => dispatch(appSlice.actions.hideSessionPlayer()),
-});
-
-const SessionPlayerBase: React.FC<
-  ReturnType<typeof mapState> & ReturnType<typeof mapDispatch>
-> = ({
-  currentUserId,
-  hideSessionPlayer,
+const SessionPlayerBase: React.FC<ReturnType<typeof mapState>> = ({
   playingSession,
   sessionPlayerVisible,
 }) => {
-  const history = useHistory();
-  usePlayingRoomStore();
-
   const agoraClient = useAgoraClient();
   useAgoraSound(agoraClient);
-  const [listening, left] = useAgoraChannelJoined(agoraClient);
-
-  const ownRoom = playingSession?.id === currentUserId;
-
-  const onMyPageClick = () => {
-    history.push(myRoomPagePath());
-  };
-
-  const onStopClick = () => {
-    leaveAgoraChannel(agoraClient);
-  };
-
-  const onCloseClick = () => {
-    hideSessionPlayer();
-  };
 
   if (!playingSession || !sessionPlayerVisible) {
     return null;
@@ -68,43 +31,10 @@ const SessionPlayerBase: React.FC<
         >
           {playingSession.name}
         </Link>
-        <div className="SessionPlayer-controls">
-          {ownRoom && (listening || left) && (
-            <button
-              className="SessionPlayer-button"
-              onClick={onMyPageClick}
-              title="My page"
-            >
-              🏠
-            </button>
-          )}
-          {!ownRoom && listening && (
-            <button
-              className="SessionPlayer-button"
-              onClick={onStopClick}
-              title="Play"
-            >
-              ⏹
-            </button>
-          )}
-          {!ownRoom && left && (
-            <button
-              className="SessionPlayer-button"
-              onClick={onCloseClick}
-              title="Close"
-            >
-              &times;
-            </button>
-          )}
-          {!listening && !left && (
-            <button className="SessionPlayer-button" disabled>
-              <span className="SessionPlayer-spinner">*</span>
-            </button>
-          )}
-        </div>
+        <SessionControls />
       </div>
     </div>
   );
 };
 
-export const SessionPlayer = connect(mapState, mapDispatch)(SessionPlayerBase);
+export const SessionPlayer = connect(mapState)(SessionPlayerBase);
