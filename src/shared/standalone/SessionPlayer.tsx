be93fa12e64db796d68a13aaeb31325631425e87
ginpei <ginpei@gmail.com>
2021-02-14 01:39:13 -0800

    fix: 🐛 channel did not close if room closed

--- src/shared/standalone/SessionPlayer.tsx
+++ src/shared/standalone/SessionPlayer.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { useErrorLog } from "../../misc/misc";
+import { useLiveRoom } from "../../models/RoomDb";
 import { userRoomPagePath } from "../../screens/users/room/UserRoomPage";
 import {
   leaveAgoraChannel,
@@ -23,8 +25,9 @@ const SessionPlayerBase: React.FC<ReturnType<typeof mapState>> = ({
   const agoraState = useAgoraConnectionState();
   useAgoraSound();
 
-  const roomOpen =
-    playingSession?.state === "open" || playingSession?.state === "live";
+  const [room, roomError] = useLiveRoom(playingSession?.id ?? null);
+  useErrorLog(roomError);
+  const roomOpen = room?.state === "open" || room?.state === "live";
 
   useEffect(() => {
     if (agoraState !== "DISCONNECTED" && !roomOpen) {
@@ -32,18 +35,15 @@ const SessionPlayerBase: React.FC<ReturnType<typeof mapState>> = ({
     }
   }, [agoraState, roomOpen]);
 
-  if (!playingSession || !sessionPlayerVisible) {
+  if (!room || !sessionPlayerVisible) {
     return null;
   }
 
   return (
     <div className="SessionPlayer">
       <div className="SessionPlayer-inner">
-        <Link
-          className="SessionPlayer-room"
-          to={userRoomPagePath(playingSession.id)}
-        >
-          {playingSession.name}
+        <Link className="SessionPlayer-room" to={userRoomPagePath(room.id)}>
+          {room.name}
         </Link>
         <SessionControls />
       </div>
