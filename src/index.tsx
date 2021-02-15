782fc4bb7864b94df9014c1ea280c9421608fbc8
ginpei <ginpei@gmail.com>
2021-02-15 01:01:45 -0800

    refactor: 💡 change index.css to scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
+import "./index.scss";
 import { initializeFirebase } from "./models/firebase";
 import reportWebVitals from "./reportWebVitals";
 
