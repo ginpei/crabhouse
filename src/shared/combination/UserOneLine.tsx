ff45f28ca827714f68ad291590c0ce02b0e98182
ginpei <ginpei@gmail.com>
2021-02-14 22:37:41 -0800

    refactor: 💡 move UserOneLine

--- src/shared/combination/UserOneLine.tsx
+++ src/shared/combination/UserOneLine.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import { User } from "../models/User";
-import { userViewPagePath } from "../screens/users/UserViewPage";
+import { User } from "../../models/User";
+import { userViewPagePath } from "../../screens/users/UserViewPage";
 import "./UserOneLine.scss";
 
 export const UserOneLine: React.FC<{ user: User }> = ({ user }) => {
